dc947897ba3cb452ce858e532e8f505bdaff40f5
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-01-18 00:26:07 -0600

    Update .js files to .ts

---
+++ src/lexer.ts
@@ -0,0 +1,18 @@
+interface Token {
+    type: string,
+    data: string
+}
+
+function tokenize(source: string): Token[] {
+    let src = source.split(""); //all the chars 
+
+    while (src.length > 0){
+
+        /* switch(src){
+            case "a" break;
+        } */
+
+    }
+
+    return /* tokens */
+}
