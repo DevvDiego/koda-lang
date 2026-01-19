0d4f181b2679f5deabaed0bca0b6659274186493
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-01-19 13:18:46 -0600

    Add TokenType enum and parse accordingly

--- src/lexer.ts
+++ src/lexer.ts
@@ -1,18 +1,67 @@
-interface Token {
-    type: string,
-    data: string
+// example of code to parse
+// let variable = mod(10);
+
+
+enum TokenType {
+    Let, //how to manage this?
+    Identifier, //how to manage this?
+    Number, //how to manage this?
+    Keyword, //how to manage this?
+    OpenParenthesis,
+    ClosingParenthesis,
+    Semicolon,
 }
 
-function tokenize(source: string): Token[] {
+interface Token {
+    type: TokenType,
+    value: string
+}
+
+// change value to be a correspondig set of defined mapped keys
+function buildToken(type: TokenType, value: string): Token {
+    return {type: type, value: value} 
+}
+
+export function tokenize(source: string): Token[] {
     let src = source.split(""); //all the chars 
+    let tokens: Token[] = [];
+    
+    while (src.length > 0) {
+        
+        switch (src[0]){ //always the first element (because we shift everything)
+        
+            case "(":
+                tokens.push(buildToken(
+                    TokenType.OpenParenthesis, 
+                    src.shift()
+                ));
+                break;
 
-    while (src.length > 0){
+            case ")":
+                tokens.push(buildToken(
+                    TokenType.ClosingParenthesis, 
+                    src.shift()
+                ));
+                break;
 
-        /* switch(src){
-            case "a" break;
-        } */
+            case ";":
+                tokens.push(buildToken(
+                    TokenType.Semicolon, 
+                    src.shift()
+                ));
+                break;
+
+            default: //all unmanaged tokens
+                tokens.push(buildToken(
+                    TokenType.Semicolon, 
+                    src.shift()
+                ));
+                break;
+        
+        } 
+            
 
     }
 
-    return /* tokens */
+    return tokens
 }
