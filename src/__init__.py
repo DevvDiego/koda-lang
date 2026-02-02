96e4f7c5bd47d1aa3a0123c23e48d8a078bd1f5b
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-02 14:14:16 -0600

    Add __init__.py to src folder to expose a more readable import name for main.py

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+#este archivo se encarga de que main.py pueda importar de forma mas facil
+#todos los "modulos" (archivos) para su uso, sin necesidad de que se dicte
+#la ruta completa
+
+#ejemplo sin __init__ en src: < from src.lexer import Lexer  >
+#ejemplo CON __iniy__ en src: < from src import Lexer >
+
+# Dentro de src/__init__.py
+from .lexer import Lexer
+#from .parser import Parser
+#from .abstract_syntax_tree import ProgramNode
