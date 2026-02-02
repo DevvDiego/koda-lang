51a87260fb0e8031aed2da6264200c0785ebc241
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-02 14:20:17 -0600

    Add Orquestrator main.py file, to manage the the compiling steps in a higher abstraction level.

---
+++ main.py
@@ -0,0 +1,26 @@
+from src import Lexer
+
+# Este archivo es el orquestrador, 
+# el punto de entrada para el compilador en general
+
+print("Koda compiler: Loading file \"main.kd\" \n\n")
+
+src = ""
+
+
+try:
+    with open("main.kd") as f:
+        src = f.read()
+
+except:
+    #cualquier error
+    print("Ocurrio un error al tratar de leer el archivo fuente")
+    print("se usara el src: 'let x = 10;'\n\n")
+    src = "let x = 10;"
+
+#a partir de aqui en teoria ya deberiamos tener el archivo en la variable src
+
+tokens = Lexer(src=src)
+
+for token in tokens:
+    print(f"tipo: {token.type}, valor: {token.value} ")
