41c9c24acefe148b33176a578776d455af07e960
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-13 23:38:49 -0600

    Refactor: use test.py to test each compiler step separatedly

--- app.py
+++ app.py
@@ -1,30 +1,3 @@
-""" from src import Lexer
-
-# Este archivo es el orquestrador, 
-# el punto de entrada para el compilador en general
-
-print("Koda compiler: Loading file \"main.kd\" \n\n")
-
-src = ""
-
-
-try:
-    with open("main.kd") as f:
-        src = f.read()
-
-except:
-    #cualquier error
-    print("Ocurrio un error al tratar de leer el archivo fuente")
-    print("se usara el src: 'int x = 10;'\n\n")
-    src = "int x = 10;"
-
-#a partir de aqui en teoria ya deberiamos tener el archivo en la variable src
-
-tokens = Lexer(src=src)
-
-for token in tokens:
-    print(f"tipo: {token.type}, valor: {token.value} ") """
-
 from flask import Flask, request, render_template
 
 app = Flask(__name__)
---
+++ test.py
@@ -0,0 +1,26 @@
+from src import Lexer
+
+# este archivo es un orquestrador mas simple que toda la interfaz de flask
+# sirve para probar parte por parte todos las fases de compilacion
+
+print("Koda compiler: Loading file \"main.kd\" \n\n")
+
+src = ""
+
+
+try:
+    with open("main.kd") as f:
+        src = f.read()
+
+except:
+    #cualquier error
+    print("Ocurrio un error al tratar de leer el archivo fuente")
+    print("se usara el src: 'int x = 10;'\n\n")
+    src = "int x = 10;"
+
+#a partir de aqui en teoria ya deberiamos tener el archivo en la variable src
+
+tokens = Lexer(src=src)
+
+for token in tokens:
+    print(f"tipo: {token.type}, valor: {token.value} ") 
