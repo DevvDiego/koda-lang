604c934c14f918413adce4424a0b296092f18047
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-14 01:10:47 -0600

    Update: ProgramNode to be a non-dataclass

--- src/models/nodes.py
+++ src/models/nodes.py
@@ -1,9 +1,19 @@
 from dataclasses import dataclass
 from typing import TypeVar, Generic
 
-@dataclass
 class ProgramNode:
-    """El contenedor raíz (la "caja" que guarda todo el archivo)"""
+    """El contenedor raíz (la "caja" que guarda todos los stmts)"""
+    def __init__(self):
+        # Una lista donde guardaremos cada Statement (sentencia) 
+        # que el parser encuentre.
+        self.sentencias = []
+
+    def add_node(self, node):
+        self.sentencias.append(node)
+
+    def __repr__(self):
+        return f"ProgramNode(sentencias={self.sentencias})"
+    
 
 @dataclass
 class Expression: 
@@ -12,14 +22,14 @@ class Expression:
 
 
 @dataclass
-class statement:
+class Statement:
     """Instrucciones que "hacen algo" (declarar, imprimir, repetir)."""
     pass
 
 
 # Definimos una Tipo generico
 T = TypeVar('T') # ignoren esta parte, es solo para type hints del IDE
-
 @dataclass
 class LiteralNode(Generic[T]):
+    """Almacena valores literales, como cadenas o numeros"""
     value: T
