28644267b5948adcf795d9cde30338838f566018
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-14 00:39:21 -0600

    Rename AST_Nodes.py to nodes.py

--- src/models/nodes.py
+++ src/models/nodes.py
@@ -1,13 +1,30 @@
+from dataclasses import dataclass
+
+@dataclass
+class Expression:
+    pass
+
+
+@dataclass
+class statement:
+    pass
+
+
+@dataclass
 class LiteralNode:
     def __init__(self, value):
         self.value = value #nodo que literalmente almacena "string" o numeros
 
+
+@dataclass
 class BinaryOpNode:
     def __init__(self, left, operand, right):
         self.left = left
         self.operand = operand
         self.right = right
 
+
+@dataclass
 class VarDeclarationNode:
     def __init__(self, type, name, expression):
         self.tipo = type
