2e4274a1141aeef04f3db1a3e4fa2806fb22343f
DevvDiego <83733071+DevvDiego@users.noreply.github.com>
2026-02-24 17:46:02 -0600

    Update: add a specific route to handle data uploading

--- app.py
+++ app.py
@@ -6,6 +6,12 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route('/handle_data', methods=['POST'])
+def handle_data():
+    # Access the form data using request.form
+    user_input = request.form['user_input_name']
+    print("user input recieved: ", user_input)
+    return f"Data received: {user_input}"
 
 # Permite hacer hot-reload y tener el servidor en modo debugging
 if __name__ == '__main__':
